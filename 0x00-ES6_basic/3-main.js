3729b95ab34ba2f5a84417af155df318e183b59f
Yinkaxlusive <97adeyinkaonatola@gmail.com>
2024-07-22 16:02:48 -0700

    basic2

---
+++ 0x00-ES6_basic/3-main.js
@@ -0,0 +1,6 @@
+import getSumOfHoods from './3-default-parameter.js';
+
+console.log(getSumOfHoods(34)); // Should output: 142 (34 + 89 + 19)
+console.log(getSumOfHoods(34, 3)); // Should output: 56 (34 + 3 + 19)
+console.log(getSumOfHoods(34, 3, 4)); // Should output: 41 (34 + 3 + 4)
+
---
+++ 0x00-ES6_basic/4-main.js
@@ -0,0 +1,5 @@
+import returnHowManyArguments from './4-rest-parameter.js';
+
+console.log(returnHowManyArguments("one")); // Should output: 1
+console.log(returnHowManyArguments("one", "two", 3, "4th")); // Should output: 4
+
