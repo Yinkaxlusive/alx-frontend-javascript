9ddaa40990aa49752f127c9d87f7bb5b32c4d239
Yinkaxlusive <97adeyinkaonatola@gmail.com>
2024-07-25 12:10:12 -0700

    update 11-createEmployeesObject.js

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,6 @@
 export default function createEmployeesObject(departmentName, employees) {
   return {
-    [departmentName]: employees
-  };
-}
-
+   const employees = {
+  employee1: 'John Doe',
+  employee2: 'Jane Doe', // Added trailing comma
+};
