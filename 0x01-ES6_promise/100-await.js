ff5e9ac1e7a57e2c759b599126125dab6ead7cc0
Yinkaxlusive <97adeyinkaonatola@gmail.com>
2024-07-25 16:15:19 -0700

    new update 100-await.js

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,13 +1,17 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   try {
-    const photoPromise = uploadPhoto();
-    const userPromise = createUser();
-
-    const [photo, user] = await Promise.all([photoPromise, userPromise]);
-    return { photo, user };
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    return {
+      photo,
+      user,
+    };
   } catch (error) {
-    return { photo: null, user: null };
+    return {
+      photo: null,
+      user: null,
+    };
   }
 }
